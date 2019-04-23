a098e23281e371b6a21a008e0a61e627e3e51eff
igharok <igharok@users.noreply.github.com>
2019-04-23 18:38:47 +0300

    2019-04-23 MRI project developing

--- HH_Modules/.ipynb_checkpoints/hh_files-checkpoint.py
+++ HH_Modules/.ipynb_checkpoints/hh_files-checkpoint.py
@@ -59,7 +59,7 @@ def hh_get_xlsx_risk_events(source_file_path, tab_name):
 
 def hh_get_country_codes():
     """
-    Version 0.01 2019-04-18
+    Version 0.02 2019-04-22
     
     FUNCTIONALITY: 
       Imports and converts country code table from url
@@ -74,13 +74,14 @@ def hh_get_country_codes():
     df_full_codes = pd.read_html(url_country_code, index_col = 'COUNTRY')[0]
     df_full_codes[['ISO SHORT', 'ISO LONG']] = df_full_codes['ISO CODES'].str.split(' / ', expand = True)
     df_result = df_full_codes[['ISO SHORT', 'ISO LONG']]      
+    df_result.index = df_result.index.str.upper()
     
     return df_result
 
 
 def hh_get_msci_index_membership(source_file_path):
     """
-    Version 0.02 2019-04-19
+    Version 0.03 2019-04-22
     
     ATTINTION: Follow the instruction of file preparing!!!
     
@@ -100,7 +101,7 @@ def hh_get_msci_index_membership(source_file_path):
     df_members.reset_index(level = 'Market Type', inplace = True)
     df_index_class = df_members['Market Type'].to_frame()
     df_index_class = df_index_class[~ df_index_class.index.duplicated()]     
-    df_index_member = df_members['Member'].str.slice(0, -4).str.capitalize().to_frame()
+    df_index_member = df_members['Member'].str.slice(0, -4).to_frame()
     df_index_member['Member Code'] = df_members['Member'].str.slice(-3, -1)
     
     return [df_index_class, df_index_member]
@@ -108,7 +109,7 @@ def hh_get_msci_index_membership(source_file_path):
 
 def hh_get_msci_reclassification(source_file_path):
     """
-    Version 0.01 2019-04-19
+    Version 0.02 2019-04-22
     
     ATTINTION: Follow the instruction of file preparing!!!
     
@@ -126,7 +127,7 @@ def hh_get_msci_reclassification(source_file_path):
     df_reclass_source.head()
 
     df_reclass_source = df_reclass_source[df_reclass_source['MARKET RECLASSIFICATION'].str.contains(' to ')]
-    df_reclass_source['Country'] = df_reclass_source['COUNTRY INDEXES'].str.replace('MSCI ', '').str.replace(' Index', '').str.replace('*', '').str.rstrip()
+    df_reclass_source['Country'] = df_reclass_source['COUNTRY INDEXES'].str.replace('MSCI ', '').str.replace(' Index', '').str.replace('*', '').str.rstrip().str.upper()
     df_reclass_source[['From Class', 'To Class']] = df_reclass_source['MARKET RECLASSIFICATION'].str.strip(' ').str.extract('(From )(.+?)(.+to )(.)')[[1, 3]] + 'M'
     df_reclass_source['Change Date'] = pd.to_datetime(df_reclass_source['DATE*'], format = '%B %Y') + pd.DateOffset(months = 1)
     df_reclass = df_reclass_source.loc[ : , 'Country' : 'Change Date']
@@ -134,18 +135,19 @@ def hh_get_msci_reclassification(source_file_path):
     return df_reclass    
 
 
-def hh_get_msci_returns(source_dir_path):
+def hh_get_msci_returns(source_dir_path, str_part_to_replace):
     """
-    Version 0.01 2019-04-19
+    Version 0.02 2019-04-23
     
     ATTINTION: Follow the instruction of files preparing!!!
     
     FUNCTIONALITY: 
-      Imports and consloidates MSCI returns info from all suitable files in current directory
+      Imports and consolidates MSCI returns info from all suitable files in current directory
     OUTPUT:
       df_returns (pd.DataFrame) - set of returns by date and country/index
     INPUT:
       source_dir_path (string) - path to the directory with source data files
+      str_part_to_replace (string) - part of column names to clear country name (usinng as str_part_to_replace + '.+')      
     """
     
     import pandas as pd
@@ -161,8 +163,50 @@ def hh_get_msci_returns(source_dir_path):
             df_from_file = df_from_file[ : df_from_file['Date'].isnull().idxmax()]        
             df_from_file['Date'] = pd.to_datetime(df_from_file['Date'], format = date_format)
             df_from_file.set_index('Date', drop = True, inplace = True)
-            df_from_file.columns = df_from_file.columns.str.extract('(.+?)( .+)')[0]
+            df_from_file.columns = df_from_file.columns.str.replace(str_part_to_replace + '.+', '').str.rstrip()
             arr_from_file.append(df_from_file)
     df_returns = pd.concat(arr_from_file, axis = 1, join = 'outer')
+    df_returns.columns.name = 'Country'
     
     return df_returns
+
+
+def hh_save_msci_returns(df_to_save, returns_freq, returns_size, returns_style, returns_suite, returns_level, returns_currency, result_file_path):
+    """
+    Version 0.01 2019-04-23
+       
+    FUNCTIONALITY: 
+     Saves msci returns to hdf file through appending with key formed as "returns_freq/returns_size/returns_style/returns_suite/returns_level/returns_currency"
+    OUTPUT:
+      returns_key (string) - key to created/appended object inside the HDF5 file
+    INPUT:
+      df_to_save (pd.DataFrame) - dataset to be stored
+      returns_freq (string) - frequency of returns ('daily' / 'monthly' / 'yearly')
+      returns_size (string) - size of capitalization for businesses to be included to index ('standart' or other sample)
+      returns_style (string) - returns style ('value' / 'growth' / 'none')
+      returns_suite (string) - specific way of index calulation ('none' or particular suite)
+      returns_level (string) - level of returns index ('price' / 'gross' / 'net')
+      returns_currency (string) - currency of returns ('USD' / 'local')      
+      result_file_path (string) - path to the hdf file      
+    """
+    
+    import pandas as pd
+    import sys 
+    sys.path.append('../..')
+    
+    from HH_Modules.hh_files import hh_get_country_codes
+    
+    df_country_codes = hh_get_country_codes()
+    
+    df_to_save_stack = df_to_save.stack().to_frame()
+    df_to_save_stack.columns = ['Returns']
+    df_to_save_stack = df_to_save_stack.merge(df_country_codes, how = 'left', left_on = 'Country', right_index = True)
+    df_to_save_stack.drop('ISO LONG', axis = 1, inplace = True)
+    df_to_save_stack.columns = ['Returns', 'Code']
+    df_to_save_stack['Code'].fillna('INDEX', inplace = True)
+    df_to_save_stack.set_index('Code', append = True, inplace = True)
+    
+    object_msci_returns_hdf = 'msci_returns_data/' + returns_freq + '/' + returns_size + '/' + returns_style + '/' + returns_suite + '/' + returns_level + '/' + returns_currency
+    df_to_save_stack.to_hdf(result_file_path, key = object_msci_returns_hdf, mode = 'a', format = 'table', append = True)
+    
+    return object_msci_returns_hdf
